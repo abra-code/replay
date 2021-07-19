02bb3064d24f350fc791c97f4b7f57765d28b9e9
Tomasz Kukielka <tom-dev@abracode.com>
2021-07-19 02:28:47 -0700

    Start 'fingerprint' project
    Xcode project setup with empty main()

---
+++ fingerprint/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  fingerprint
+//
+//  Created by Tomasz Kukielka on 7/19/21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+	printf("'fingerprint' is a verb\n");
+	return 0;
+}
